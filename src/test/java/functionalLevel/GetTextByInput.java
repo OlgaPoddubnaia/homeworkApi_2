80f1eb6820f5153aaccb8e7cf51e382689fea755
OlgaPoddubnaia <catdance@mail.ru>
2021-02-02 21:10:34 +0300

    started to build project

--- src/test/java/functionalLevel/GetTextByInput.java
+++ src/test/java/functionalLevel/GetTextByInput.java
@@ -1,25 +1,21 @@
 package functionalLevel;
 
 import com.google.gson.Gson;
-import io.restassured.mapper.ObjectMapper;
 import org.testng.annotations.DataProvider;
 import rename.dto.SpellerIncomeForText;
 import rename.dto.SpellerIncomeForTexts;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.Map;
+
 
 public class GetTextByInput {
 
     private String jsonPathText = "src/test/resources/forCheckTextData.json";
     private String jsonPathTexts = "src/test/resources/forCheckTextsData.json";
-    prvivate Gson gson;
-    private static FileInputStream fileInputStream;
-    private static ObjectMapper objectMapper;
-    private static Object[][] dataObject;
+    private Gson gson;
+    private FileReader fileReader;
+    private Object[][] dataObject;
     private SpellerIncomeForText[] spellerIncomeForText;
     private SpellerIncomeForTexts[] spellerIncomeForTexts;
 
@@ -28,21 +24,22 @@ public class GetTextByInput {
 
         gson = new Gson();
         try {
-            fr = new FileReader(path);
-            if (path.equals(filePathToTextData)) {
-                textDataArr = gson.fromJson(fr, DataForText[].class);
-                dataProviderArray = new Object[textDataArr.length][1];
-                for (int j = 0; j < textDataArr.length; j++) {
-                    dataProviderArray[j][0] = textDataArr[j];
+            fileReader = new FileReader(path);
+            if (path.equals(jsonPathText)) {
+                spellerIncomeForText = gson.fromJson(fileReader, SpellerIncomeForText[].class);
+                dataObject = new Object[spellerIncomeForText.length][1];
+                for (int j = 0; j < spellerIncomeForText.length; j++) {
+                    dataObject[j][0] = spellerIncomeForText[j];
                 }
             }
-            if (path.equals(filePathToTextsData)) {
-                dataForTexts = gson.fromJson(fr, DataForTexts[].class);
-                dataProviderArray = new Object[dataForTexts.length][1];
-                for (int j = 0; j < dataForTexts.length; j++) {
-                    dataProviderArray[j][0] = dataForTexts[j];
+            if (path.equals(jsonPathTexts)) {
+                spellerIncomeForTexts = gson.fromJson(fileReader, SpellerIncomeForTexts[].class);
+                dataObject = new Object[spellerIncomeForTexts.length][1];
+                for (int j = 0; j < spellerIncomeForTexts.length; j++) {
+                    dataObject[j][0] = spellerIncomeForTexts[j];
                 }
             }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
